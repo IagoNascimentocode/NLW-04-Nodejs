7f6b2363b1ec6df08d9c5397af862bdcfabf1eed
IagoNascimentocode <iago_ns@hotmail.com>
2021-02-23 16:00:41 -0300

    Segundo dia - Banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database'
+import { router } from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
 
-    return res.json({ message: "Hellow world - NLW 04 " })
-})
-
-app.post('/', (req, res) => {
-    return res.json({ message: "Os dados foram salvos com sucesso!" })
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => { console.log("Server run in port: 3333") })
 
